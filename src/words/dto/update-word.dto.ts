c48de8c1330dae63f6ad750adf93f354ebeb1fd8
cola119 <k.cola119@ezweb.ne.jp>
2020-08-27 23:23:14 +0900

    add DTO validator

--- src/words/dto/update-word.dto.ts
+++ src/words/dto/update-word.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, MaxLength, IsString } from 'class-validator';
+
 export class UpdateWordDto {
+  @IsNotEmpty()
+  @MaxLength(255)
+  @IsString()
   str: string;
+
+  @IsNotEmpty()
+  @MaxLength(255)
+  @IsString()
   meaning: string;
 }
