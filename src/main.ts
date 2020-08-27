d1f12e8ec78ee2531612f9d1244db5f69846622b
cola119 <k.cola119@ezweb.ne.jp>
2020-08-27 23:42:54 +0900

    secure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet);
   await app.listen(3000);
 }
 bootstrap();
