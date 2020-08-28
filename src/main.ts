ab4fe2fb513946263c0681b80e5b3f9f8cab94d1
cola119 <k.cola119@ezweb.ne.jp>
2020-08-29 00:10:49 +0900

    fix invalid syntax

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  app.use(helmet);
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
